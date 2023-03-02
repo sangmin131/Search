a3056f89724092b87c5b224a1ca0cdf6b46a0fb1
sangmin131 <smha77@naver.com>
2023-03-02 12:07:02 +0900

    로그인기능을 구현중입니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import './App.css';
 import SearchH from './SearchH';
+
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-<Outlet/>
 <SearchH/>
+<QueryClientProvider client={queryClient}>
+<Outlet/>
+</QueryClientProvider>
     </>
   );
 }
--- src/page/Videos.jsx
+++ src/page/Videos.jsx
@@ -1,9 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from "react-router-dom";
+import FackYoutube from '../api/fakeYoutube';
+import { search } from '../api/youtube';
+import VideoCard from '../VideoCard';
+
+
 export default function Videos() {
   const {keyword} = useParams();
+  const {isLoadung, error, data: videos} = useQuery(
+    ['videos', keyword], () => {
+      const youtube = new FackYoutube();
+      return youtube.search(keyword);
+    }
+  );
   return (
+    <>
     <div>검색결과 {keyword ? `😎${keyword}`:'🥺'}</div>
+    {isLoadung && <p>Loading...</p>}
+    {error && <p>Something is wrong😫</p>}
+    {videos && <ul>
+      {videos.map(video => <VideoCard key={video.id} video = {video}/> ) }
+      </ul>}
+    </>
   );
 }
 
